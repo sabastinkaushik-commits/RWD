262b575ad6c100db5a3a0809d01f92c2d9c0ed20
sabastinkaushik-commits <sabastinkaushik@gmail.com>
2026-01-08 20:43:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Show feature clicked
+function showFeature(featureName) {
+    alert(featureName + " is opening...");
+}
+
+// Dark/Light Mode Toggle
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+}
+
+// Console greeting
+console.log("Advanced Dashboard Loaded Successfully!");
